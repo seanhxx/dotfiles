2d9cf467614cad140b6d4d184d260d203fa88963
Hu Xiaoxiang <seanhxx42@live.com>
2025-07-29 14:31:20 +0800

    Update flake.lock: refresh dependencies and update lastModified timestamps Update default.nix: switch to latest kernel packages for improved hardware support Update virtualisation.nix: comment out virt-top package in systemPackages

--- nixos-configurations/work/default.nix
+++ nixos-configurations/work/default.nix
@@ -141,7 +141,7 @@
 
   boot = {
     # Use the latest kernel for best hardware support and performance
-    kernelPackages = pkgs.linuxPackages_6_13;
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelModules = [
       "evdi"
     ];
